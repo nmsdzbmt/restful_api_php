00f9054b6bf69a1858a8b30e1f56886b12f2bdf7
nmsdzbmt <nmsdzbmt@gmail.com>
2021-12-12 16:39:34 +0700

    Update and Delete  APi

--- model/question.php
+++ model/question.php
@@ -69,6 +69,58 @@
                 printf("Error %s. \n" ,$stmt->error);
                 return false;
             }
-        }     
+        }
+        
+        //update data
+        public function update(){
+            $query = "UPDATE cauhoi 
+            SET title=:title, cau_a=:cau_a, cau_b=:cau_b, cau_c=:cau_c, cau_d=:cau_d, cau_dung=:cau_dung 
+            WHERE id=:id";
+            $stmt = $this->conn->prepare($query);
+
+            //clean data: Lọc kí tự đặc biệt
+            $this->title = htmlspecialchars(strip_tags($this->title));
+            $this->cau_a = htmlspecialchars(strip_tags($this->cau_a));
+            $this->cau_b = htmlspecialchars(strip_tags($this->cau_b));
+            $this->cau_c = htmlspecialchars(strip_tags($this->cau_c));
+            $this->cau_d = htmlspecialchars(strip_tags($this->cau_d));
+            $this->cau_dung = htmlspecialchars(strip_tags($this->cau_dung));
+            $this->id = htmlspecialchars(strip_tags($this->id));
+
+            //bind data
+            $stmt->bindParam(':title', $this->title);
+            $stmt->bindParam(':cau_a', $this->cau_a);
+            $stmt->bindParam(':cau_b', $this->cau_b);
+            $stmt->bindParam(':cau_c', $this->cau_c);
+            $stmt->bindParam(':cau_d', $this->cau_d);
+            $stmt->bindParam(':cau_dung', $this->cau_dung);
+            $stmt->bindParam(':id', $this->id);
+            
+            if($stmt->execute()){
+                return true;
+            }else{
+                printf("Error %s. \n" ,$stmt->error);
+                return false;
+            }
+        }
+        
+        //delete data
+        public function delete(){
+            $query = "DELETE FROM cauhoi WHERE id=:id";
+            $stmt = $this->conn->prepare($query);
+
+            //clean data: Lọc kí tự đặc biệt
+            $this->id = htmlspecialchars(strip_tags($this->id));
+
+            //bind data
+            $stmt->bindParam(':id', $this->id);
+            
+            if($stmt->execute()){
+                return true;
+            }else{
+                printf("Error %s. \n" ,$stmt->error);
+                return false;
+            }
+        }    
     }
 ?>
