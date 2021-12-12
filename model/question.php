905b8d230a25e2b837e2b54f6200988a5f72c6b0
nmsdzbmt <nmsdzbmt@gmail.com>
2021-12-12 15:30:41 +0700

    Read and show APi

--- model/question.php
+++ model/question.php
@@ -24,5 +24,22 @@
             $stmt->execute();
             return $stmt;
         }
+
+        //show data
+        public function show(){
+            $query = "SELECT * FROM cauhoi WHERE id=? LIMIT 1";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1, $this->id);
+            $stmt->execute();
+            
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->title = $row['title'];
+            $this->cau_a = $row['cau_a'];
+            $this->cau_b = $row['cau_b'];
+            $this->cau_c = $row['cau_c'];
+            $this->cau_d = $row['cau_d'];
+            $this->cau_dung = $row['cau_dung'];
+        }
     }
 ?>
